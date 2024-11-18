4241924dc37352d46cc1bf2105b1dc6564ff3ee1
Vigneshwar N <vigneshwar.doodleblue@gmail.com>
2024-11-18 19:48:18 +0530

    add + - to detail

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,11 +1,21 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {hp, wp} from '../utility/responsive';
-import {data} from '../data/data';
+import {useDispatch, useSelector} from 'react-redux';
+import {addToCart, removeItem} from '../reduxToolkit/cartSlice';
 
 export default function Details({route}) {
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.items);
   const {item} = route.params;
-  const {products} = data;
+  const cartItem = cartItems.find(cartItem => cartItem.id === item.id);
+
+  function addItem() {
+    dispatch(addToCart(item));
+  }
+  function removeItems(item) {
+    dispatch(removeItem(item));
+  }
 
   return (
     <View style={{backgroundColor: '#FFF', flex: 1}}>
@@ -51,11 +61,69 @@ export default function Details({route}) {
             paddingTop: hp(1),
             fontSize: 26,
           }}>
-          {`$${item.price}`}
+          {cartItem ? `$${Math.round(cartItem.totalPrice)}` : `$${item.price}`}
         </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity onPress={addItem}>
+          <Text
+            style={{
+              padding: '2%',
+              borderWidth: 1,
+              height: 50,
+              width: 50,
+              verticalAlign: 'middle',
+              textAlign: 'center',
+              borderRadius: 100,
+              backgroundColor: '#000',
+              color: '#FFF',
+              fontSize: 20,
+              fontWeight: 'bold',
+            }}>
+            +
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            padding: '2%',
+            height: 50,
+            width: 50,
+            verticalAlign: 'middle',
+            textAlign: 'center',
+            borderRadius: 100,
+            color: '#000',
+            fontSize: 20,
+            fontWeight: 'bold',
+          }}>
+          {cartItem ? `${cartItem.quantity}` : '0'}
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            removeItems(item.id);
+          }}>
+          <Text
+            style={{
+              padding: '2%',
+              borderWidth: 1,
+              height: 50,
+              width: 50,
+              verticalAlign: 'middle',
+              textAlign: 'center',
+              borderRadius: 100,
+              backgroundColor: '#000',
+              color: '#FFF',
+              fontSize: 20,
+              fontWeight: 'bold',
+            }}>
+            -
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
